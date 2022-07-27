b08ddd4bdd199f9f03badf41f97a1253930e47ac
astifaw <110075446+astifaw@users.noreply.github.com>
2022-07-27 14:43:46 +0700

    Update tes1.py

--- tes1.py
+++ tes1.py
@@ -14,7 +14,7 @@ class TestLogin(unittest.TestCase):
         driver = self.driver
         driver.get("http://barru.pythonanywhere.com/daftar") # buka situs
         time.sleep(3)
-        driver.find_element(By.ID,"email").send_keys("tester@jagoqa.comx") # isi email
+        driver.find_element(By.ID,"email").send_keys("tester@jagoqa.com") # isi email
         time.sleep(1)
         driver.find_element(By.ID,"password").send_keys("testerjago") # isi password
         time.sleep(1)
@@ -43,7 +43,7 @@ class TestLogin(unittest.TestCase):
         response_message = driver.find_element(By.ID,"swal2-content").text
 
         self.assertEqual(response_data, 'Email tidak valid')
-        self.assertEqual(response_message, 'Anda Berhasil Login')
+        self.assertEqual(response_message, 'Cek kembali email anda')
 
     def test_c_failed_login_with_empty_email(self):
         print ("tulis disini test case mu")
