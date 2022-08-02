1e02deba924c421b04e9de95cdea3975173ebabc
astifaw <110075446+astifaw@users.noreply.github.com>
2022-08-02 20:07:16 +0700

    trial n error

--- Final_Project/OrangeHRM2.py
+++ Final_Project/OrangeHRM2.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 #variable
-url="https://opensource-demo.orangehrmlive.com/index.php/admin/viewSystemUsers"
+url="https://opensource-demo.orangehrmlive.com/index.php/admin/saveJobTitle"
 username="Admin" # username admin
 password="admin123" # password admin
 
@@ -23,22 +23,22 @@ class TestLogin(unittest.TestCase):
         driver.find_element(By.ID,"txtPassword").send_keys(password)
         time.sleep(1)
         driver.find_element(By.ID,"btnLogin").click()
-        time.sleep(1)
+        time.sleep(2)
 
     def test_search(self):
         driver = self.driver
         self.test_login()
         time.sleep(3)
-        driver.find_element(By.ID,"searchSystemUser_userName").send_keys("linda")
+        driver.find_element(By.ID,"jobTitle_jobTitle").send_keys("Job Title 5")
         time.sleep(3)
-        driver.find_element(By.ID,"searchBtn").click()
-        time.sleep(3)
-        driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/div[3]/div[2]/div[1]/div[1]/form[1]/div[4]/table[1]/tbody[1]/tr[1]/td[1]/input[1]").click()
-        time.sleep(3)
+        driver.find_element(By.ID,"jobTitle_jobDescription").send_keys("Drive company's cars")
+        time.sleep(1)
+        driver.find_element(By.ID,"btnSave").click()
+        time.sleep(5)
         
-        response_data = driver.find_element(By.XPATH,"/html/body/div[1]/div[3]/div[2]/div/div/form/div[4]/table/tbody/tr/td[2]/a").text
+        response_data = driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/div[3]/div[1]/div[1]/div[2]/form[1]/div[4]/table[1]/tbody[1]").text
         
-        self.assertEqual(response_data, 'linda')
+        self.assertEqual(response_data, 'Job Title 5')
     
     def tearDown(self): 
         self.driver.close() 
--- Final_Project/OrangeHRM3.py
+++ Final_Project/OrangeHRM3.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 #variable
-url="https://opensource-demo.orangehrmlive.com/index.php/admin/viewSystemUsers"
+url="https://opensource-demo.orangehrmlive.com/index.php/leave/viewLeaveList/"
 username="Admin" # username admin
 password="admin123" # password admin
 
@@ -23,22 +23,18 @@ class TestLogin(unittest.TestCase):
         driver.find_element(By.ID,"txtPassword").send_keys(password)
         time.sleep(1)
         driver.find_element(By.ID,"btnLogin").click()
-        time.sleep(1)
+        time.sleep(2)
 
-    def test_search(self):
+    def test_search_leave(self):
         driver = self.driver
         self.test_login()
         time.sleep(3)
-        driver.find_element(By.ID,"searchSystemUser_userName").send_keys("linda")
-        time.sleep(3)
-        driver.find_element(By.ID,"searchBtn").click()
-        time.sleep(3)
-        driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/div[3]/div[2]/div[1]/div[1]/form[1]/div[4]/table[1]/tbody[1]/tr[1]/td[1]/input[1]").click()
+        driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/div[3]/div[1]/div[2]/form[1]/fieldset[1]/ol[1]/li[3]/div[1]/label[1]").click()
         time.sleep(3)
         
-        response_data = driver.find_element(By.XPATH,"/html/body/div[1]/div[3]/div[2]/div/div/form/div[4]/table/tbody/tr/td[2]/a").text
+        #response_data = driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/div[3]/div[1]/div[1]/div[2]/form[1]/div[4]/table[1]/tbody[1]").text
         
-        self.assertEqual(response_data, 'linda')
+        #elf.assertEqual(response_data, 'Job Title 5')
     
     def tearDown(self): 
         self.driver.close() 
