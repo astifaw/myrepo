c3952a11bb783c81e78c44ff299a40a398e81f5d
astifaw <110075446+astifaw@users.noreply.github.com>
2022-08-04 20:09:12 +0700

    -

--- Test-OrangeHRM(BELUM-SELESAI)/10_Buzz/Test-Buzz_Post.py
+++ Test-OrangeHRM(BELUM-SELESAI)/10_Buzz/Test-Buzz_Post.py
@@ -1,41 +1,44 @@
+import numbers
 import unittest
 import time
+from urllib import response
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
 
 #variable
-url="https://opensource-demo.orangehrmlive.com/index.php/leave/viewLeaveList/"
+url="https://opensource-demo.orangehrmlive.com/index.php/buzz/viewBuzz" #URL langsung ke menu
 username="Admin" # username admin
 password="admin123" # password admin
 
-class TestLogin(unittest.TestCase): 
+class Test_Buzz(unittest.TestCase): 
 
     def setUp(self): 
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
-        
-    def test_login(self):
         driver = self.driver
         driver.get(url) # buka situs
         time.sleep(3)
         driver.find_element(By.ID,"txtUsername").send_keys(username)
-        time.sleep(3)
+        time.sleep(1)
         driver.find_element(By.ID,"txtPassword").send_keys(password)
         time.sleep(1)
         driver.find_element(By.ID,"btnLogin").click()
-        time.sleep(2)
+        time.sleep(3)
 
-    def test_search_leave(self):
+    def test_A_Buzz_Post_Status (self):
         driver = self.driver
-        self.test_login()
-        time.sleep(3)
-        driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/div[3]/div[1]/div[2]/form[1]/fieldset[1]/ol[1]/li[3]/div[1]/label[1]").click()
+        driver.find_element(By.ID,"createPost_content").send_keys("Have a good day everyone....!!!")
+        time.sleep(1)
+        driver.find_element(By.ID,"postSubmitBtn").click()
         time.sleep(3)
         
-        #response_data = driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/div[3]/div[1]/div[1]/div[2]/form[1]/div[4]/table[1]/tbody[1]").text
-        
-        #elf.assertEqual(response_data, 'Job Title 5')
-    
+        response_name = driver.find_element(By.XPATH,"/html[1]/body[1]/div[1]/div[3]/div[1]/div[6]/ul[1]/li[1]/div[1]/div[5]/div[1]").text
+
+        self.assertEqual(response_name,'Have a good day everyone....!!!')
+
     def tearDown(self): 
         self.driver.close() 
 
